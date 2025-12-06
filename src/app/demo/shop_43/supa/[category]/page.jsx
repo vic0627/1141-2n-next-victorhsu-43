27bd338fc3ad3198a63d5ba449fe32578a80276a
victor_xu <pure90719@gmail.com>
2025-12-06 18:12:56 +0800

    add supa

--- src/app/demo/shop_43/supa/[category]/page.jsx
+++ src/app/demo/shop_43/supa/[category]/page.jsx
@@ -1,43 +1,58 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import { supabase } from "@/db/clientSupabase";
 
-import Wrapper from '@/assets/wrappers/midterm/Shop_43';
-import Product_43 from '@/components/midterm/Product_43';
+import Wrapper from "@/assets/wrappers/midterm/Shop_43";
+import Product_43 from "@/components/midterm/Product_43";
 
 const FetchShopByCategory_43 = () => {
   const [shop_43, setShop_43] = useState([]);
   const params = useParams();
   const category = params.category;
-  console.log('category', category);
+  console.log("category", category);
 
-  const fetchShopFromNode = async () => {
+  const fetchShopFromSupaServer = async () => {
     try {
-      const res = await fetch(`http://localhost:5000/api/shop_43/${category}`)
-      const data = await res.json()
-      console.log('shop_43_data', data)
-      if (data.length) {
-        setShop_43(data)
-      }
+      const { data: cat } = await supabase
+        .from("category2_43")
+        .select("cid")
+        .eq("cname", category)
+        .single();
+      const { data } = await supabase
+        .from("shop2_43")
+        .select("*")
+        .eq("cat_id", cat.cid);
+      console.log("data", data);
+      setShop_43(data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchShopFromNode()
-  }, [])
+    fetchShopFromSupaServer();
+  }, []);
 
   return (
     <Wrapper>
-      <div className='shop-page'>
-        <div className='section-title'>
+      <div className="shop-page">
+        <div className="section-title">
           <h3>Victor Hsu, 213410243</h3>
         </div>
-        <div className='collection-page'>
-          <h1 className='title'>{category[0].toUpperCase() + category.slice(1)}</h1>
-          <div className='items'>
-            {shop_43.map(({ pid, pname, img_url, price }) => <Product_43 key={pid} name={pname} img_url={img_url} price={price} />)}
+        <div className="collection-page">
+          <h1 className="title">
+            {category[0].toUpperCase() + category.slice(1)}
+          </h1>
+          <div className="items">
+            {shop_43.map(({ pid, pname, img_url, price }) => (
+              <Product_43
+                key={pid}
+                name={pname}
+                img_url={img_url}
+                price={price}
+              />
+            ))}
           </div>
         </div>
       </div>
